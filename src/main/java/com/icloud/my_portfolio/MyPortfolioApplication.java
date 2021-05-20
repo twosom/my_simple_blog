6dfd9d40d4f39538c621cdf71f85b82b4fb72764
desirel <two_somang@icloud.com>
2021-05-21 02:49:13 +0900

    redis setting

--- src/main/java/com/icloud/my_portfolio/MyPortfolioApplication.java
+++ src/main/java/com/icloud/my_portfolio/MyPortfolioApplication.java
@@ -4,7 +4,14 @@ package com.icloud.my_portfolio;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -12,6 +19,7 @@ import org.springframework.stereotype.Component;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 import javax.persistence.EntityManager;
+import java.time.Duration;
 
 
 @EnableRedisHttpSession
@@ -41,6 +49,17 @@ public class MyPortfolioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyPortfolioApplication.class, args);
+    }
+
+    // REDIS BEAN
+    public CacheManager userCacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMillis(3L));
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
+                .cacheDefaults(redisCacheConfiguration).build();
     }
 
 }
