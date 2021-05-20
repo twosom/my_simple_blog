9d25d7e2726d7358f96248799b3a209d5eb60830
desirel <two_somang@icloud.com>
2021-05-21 03:24:11 +0900

    no message

--- src/main/java/com/icloud/my_portfolio/MyPortfolioApplication.java
+++ src/main/java/com/icloud/my_portfolio/MyPortfolioApplication.java
@@ -4,27 +4,16 @@ package com.icloud.my_portfolio;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 import javax.persistence.EntityManager;
-import java.time.Duration;
 
 
-@EnableRedisHttpSession
-@EnableCaching
+
 @SpringBootApplication
 @Component
 public class MyPortfolioApplication {
---
+++ src/main/java/com/icloud/my_portfolio/config/SecurityInitializer.java
@@ -0,0 +1,12 @@
+package com.icloud.my_portfolio.config;
+
+
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    public SecurityInitializer() {
+        super(SecurityConfig.class, Config.class);
+    }
+}
