10d93f3dba3af76e654ff56049550d515945f5e4
desirel <two_somang@icloud.com>
2021-05-21 02:59:14 +0900

    SERIALIZABLE

--- src/main/java/com/icloud/my_portfolio/service/UserService.java
+++ src/main/java/com/icloud/my_portfolio/service/UserService.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -25,7 +26,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class UserService implements UserDetailsService {
+public class UserService implements UserDetailsService, Serializable {
 
     private final UserJpaRepository userJpaRepository;
     private final PasswordEncoder passwordEncoder;
